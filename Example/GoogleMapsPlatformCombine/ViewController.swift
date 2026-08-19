e0a884c4291259c6a1dcda86c97d1402b9edfc3d
Ed Boiling <edboiling@google.com>
2026-08-19 16:02:26 +0100

    chore: update deprecated GMSMapView initializer to GMSMapViewOptions
    - Replaced deprecated GMSMapView.map(withFrame:camera:) with GMSMapViewOptions in Example/GoogleMapsPlatformCombine/ViewController.swift
    - Updated GoogleMaps and GooglePlaces minimum dependencies in podspec and Podfile to modern SDK versions (iOS 15.0+ deployment target)
    - Updated project deployment target settings in Example Xcode project

--- Example/GoogleMapsPlatformCombine/ViewController.swift
+++ Example/GoogleMapsPlatformCombine/ViewController.swift
@@ -33,7 +33,10 @@ class ViewController: UIViewController {
       // Create a GMSCameraPosition that tells the map to display the
       // coordinate -33.86,151.20 at zoom level 6.
       let camera = GMSCameraPosition.camera(withLatitude: sydneyLatitude, longitude: sydneyLongitude, zoom: 6.0)
-      let mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
+      let options = GMSMapViewOptions()
+      options.camera = camera
+      options.frame = view.frame
+      let mapView = GMSMapView(options: options)
       view.addSubview(mapView)
 
       // Creates a marker in the center of the map.
